c8d414122c0ecd91b9310353a8fa6185cfa152bf
Aerial <retar@amretar.com>
2026-07-31 00:34:26 -0500

    oh wait the builds work on both platforms
    literally the title, i realized after commit that both builds work on both platforms now, so there is no need to segregate (except for the editor for now)

--- CS/build.bat
+++ CS/build.bat
@@ -1,4 +1,4 @@
 @echo off
 cls
-dotnet publish "Angene.sln" -c Release -r win-x64 -o "..\Build\win-x64\Angene"
+dotnet publish "Angene.sln" -c Release -o "..\Build\Angene"
 dotnet publish "AngeneEditor\AngeneEditor.sln" -c Release -r win-x64 -o "..\Build\win-x64\AngeneEditor"
